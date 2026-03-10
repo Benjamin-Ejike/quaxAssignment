31677816359cb7cb2e10d16f09d26cb5afda8090
Koseimasumi <kosei.masumi@ucdconnect.ie>
2026-03-10 16:36:17 +0000

    Add files via upload
    Pie rule final version

--- Game.java
+++ Game.java
@@ -3,27 +3,17 @@ public class Game {
     private Board board;
     private Colour currentPlayer;
 
-    // Track game state for Pie Rule
-    private int moveCount;
-    private boolean pieRuleUsed;
-
     public Game() {
         startGame();
     }
 
     public void startGame() {
         board = new Board();
-        currentPlayer = Colour.BLACK; // BLACK starts
-        moveCount = 0;
-        pieRuleUsed = false;
+        currentPlayer = Colour.BLACK; // BLACK starts (spec)
     }
 
     public Colour getCurrentPlayer() {
         return currentPlayer;
-    }
-
-    public Board getBoard() {
-        return board;
     }
 
     public void switchTurn() {
@@ -34,7 +24,6 @@ public class Game {
         }
     }
 
-    // Place stone
     public boolean placeStone(int row, int col) {
 
         if (!board.isValidPosition(row, col)) {
@@ -44,34 +33,15 @@ public class Game {
         Cell cell = board.getCell(row, col);
 
         if (!cell.isEmpty()) {
-            return false;
+            return false; // illegal move (occupied)
         }
 
         cell.setColour(currentPlayer);
-
-        moveCount++;
-
         switchTurn();
-
         return true;
     }
 
-    // Apply Pie Rule
-    public void applyPieRule() {
-
-        if (moveCount == 1 && !pieRuleUsed) {
-
-            switchTurn();
-
-            pieRuleUsed = true;
-        }
-    }
-
-    public boolean isFirstMove() {
-        return moveCount == 1;
-    }
-
-    public boolean isPieRuleUsed() {
-        return pieRuleUsed;
+    public Board getBoard() {
+        return board;
     }
 }
