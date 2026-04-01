63f96f6e3c8609809c6eead66582775876ed3867
Koseimasumi <kosei.masumi@ucdconnect.ie>
2026-04-01 21:12:01 +0100

    Add files via upload

--- Board.java
+++ Board.java
@@ -1,55 +1,46 @@
 //creates and manages the board
-public class Board {
 
+public class Board {
     private static final int SIZE = 11;
-    private Cell [][] grid ;
+    private Cell[][] grid;
 
     public Board() {
-        createBoard();
+        grid = new Cell[SIZE][SIZE];
+        for(int r = 0; r < SIZE; r++) {
+            for(int c = 0; c < SIZE; c++) grid[r][c] = new Cell(r, c);
+        }
     }
 
-    //create an empty 11x11 board
-    public void createBoard() {
-        grid = new Cell[SIZE][SIZE];
-        for(int row = 0; row < SIZE; row++) {
-            for(int col = 0; col < SIZE; col++) {
-                grid[row][col] = new Cell(row, col);
-            }
-        }
-    }
     //get specific cell
-    public Cell getCell(int row, int col) {
-        if (isValidPosition(row,col)){
-            return grid[row][col];
-        }
+    public Cell getCell(int r, int c) {
+        if (r >= 0 && r < SIZE && c >= 0 && c < SIZE) return grid[r][c];
         return null;
     }
-    //check if the coordinates are insde the board
-    public boolean isValidPosition(int row, int col) {
-        return (row >= 0 && row < SIZE &&
-                col >= 0 && col < SIZE);
-    }
 
-    public boolean isCellEmpty(int row, int col) {
-        if (!isValidPosition(row,col)) {
-            return false;
-        }
-        return grid[row][col].isEmpty();
+    public boolean isCellEmpty(int r, int c) {
+        Cell cell = getCell(r, c);
+        return cell != null && cell.isEmpty();
     }
 
     //place a stone only of its inside theboard and is empty
-    public boolean placeStone(int row, int col, Colour colour) {
-        if (!isValidPosition(row,col)) {
-            return false;
+    public boolean placeStone(int r, int c, Colour color) {
+        if (isCellEmpty(r, c)) {
+            grid[r][c].setColour(color);
+            return true;
         }
-        if (!isCellEmpty(row,col)) {
-            return false;
-        }
-        grid[row][col].setColour(colour);
-        return true;
-    }
-    public int getSize() {
-        return SIZE;
+        return false;
     }
 
+    //Swap colour when pie rule is activated
+    public void swapAllColours() {
+        for(int r = 0; r < SIZE; r++) {
+            for(int c = 0; c < SIZE; c++) {
+                if (!grid[r][c].isEmpty()) {
+                    grid[r][c].setColour(grid[r][c].getColor() == Colour.BLACK ? Colour.WHITE : Colour.BLACK);
+                }
+            }
+        }
+    }
+
+    public int getSize() { return SIZE; }
 }
